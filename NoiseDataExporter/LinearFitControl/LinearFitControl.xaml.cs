cb3bfe503362df05f9d74b45dcbd68e7d0de8d1f
zigorrom <zigorrom@gmail.com>
2015-03-20 19:30:22 +0100

    123

---
+++ NoiseDataExporter/LinearFitControl/LinearFitControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Research.DynamicDataDisplay.Charts;
+using Microsoft.Research.DynamicDataDisplay.Charts.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinearFitControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class LinearFitControl : UserControl
+    {
+
+        private LinearFitViewModel m_viewModel;
+        public LinearFitControl()
+        {
+            InitializeComponent();
+            m_viewModel = new LinearFitViewModel();
+            var lp = new Point(0, 0);
+            var rp = new Point(1, 0);
+            var LeftDraggablePoint = new DraggablePoint(lp);
+            var RightDraggablePoint = new DraggablePoint(rp);
+            LeftDraggablePoint.PositionChanged += leftDraggablePoint_PositionChanged;
+            RightDraggablePoint.PositionChanged += rightDraggablePoint_PositionChanged;
+
+            m_viewModel.LeftMarkerPosition = lp.X;
+            m_viewModel.RightMarkerPosition = rp.X;
+
+            
+            LinearFitPlotter.Children.Add(LeftDraggablePoint);
+            LinearFitPlotter.Children.Add(RightDraggablePoint);
+
+            var LeftVerticalLineBinding = new Binding("LeftMarkerPosition");
+            LeftVerticalLineBinding.Source = m_viewModel;
+            LeftVerticalLine.SetBinding(VerticalLine.ValueProperty, LeftVerticalLineBinding);
+
+            var RigthVerticalLineBinding = new Binding("RightMarkerPosition");
+            RigthVerticalLineBinding.Source = m_viewModel;
+            RightVerticalLine.SetBinding(VerticalLine.ValueProperty, RigthVerticalLineBinding);
+        }
+        void rightDraggablePoint_PositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            m_viewModel.RightMarkerPosition = e.Position.X;
+        }
+
+        void leftDraggablePoint_PositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            m_viewModel.LeftMarkerPosition = e.Position.X;
+        }
+
+        private void DoneButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+        }
+    }
+}
---
+++ NoiseDataExporter/LinearFitControl/LinearFitViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LinearFitControl
+{
+    public class LinearFitViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string PropertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double m_DSvoltage;
+
+        public double DSVoltage
+        {
+            get { return m_DSvoltage; }
+            set
+            {
+                if (m_DSvoltage == value)
+                    return;
+                m_DSvoltage = value;
+                OnPropertyChanged("DSVoltage");
+            }
+        }
+
+        private double m_TresholdVoltage;
+
+        public double TresholdVoltage
+        {
+            get { return m_TresholdVoltage; }
+            set
+            {
+                if (m_TresholdVoltage == value)
+                    return;
+                m_TresholdVoltage = value;
+                OnPropertyChanged("TresholdVoltage");
+            }
+        }
+
+
+        private Point m_LeftDraggablePointPosition;
+
+        public Point LeftDraggablePointPosition
+        {
+            get { return m_LeftDraggablePointPosition; }
+            set
+            {
+                if (m_LeftDraggablePointPosition == value)
+                    return;
+                if (value.X > m_RightDraggablePointPosition.X)
+                    m_LeftDraggablePointPosition = new Point(m_LeftDraggablePointPosition.X, value.Y);
+                m_LeftDraggablePointPosition = value;
+                m_LeftMarkerPosition = m_LeftDraggablePointPosition.X;
+                OnPropertyChanged("LeftDraggablePointPosition");
+            }
+        }
+
+        private Point m_RightDraggablePointPosition;
+
+        public Point RightDraggablePointPosition
+        {
+            get { return m_RightDraggablePointPosition; }
+            set
+            {
+                if (m_RightDraggablePointPosition == value)
+                    return;
+                if (value.X < m_LeftDraggablePointPosition.X)
+                    m_RightDraggablePointPosition = new Point(m_RightDraggablePointPosition.X, value.Y);
+                m_RightDraggablePointPosition = value;
+                m_RightMarkerPosition = m_RightDraggablePointPosition.X;
+                OnPropertyChanged("RightDraggablePointPosition");
+            }
+        }
+
+
+        private double m_LeftMarkerPosition;
+
+        public double LeftMarkerPosition
+        {
+            get { return m_LeftMarkerPosition; }
+            set
+            {
+                if (m_LeftMarkerPosition == value)
+                    return;
+                m_LeftMarkerPosition = value;
+                OnPropertyChanged("LeftMarkerPosition");
+
+            }
+        }
+
+        private double m_RightMarkerPosition;
+
+        public double RightMarkerPosition
+        {
+            get { return m_RightMarkerPosition; }
+            set
+            {
+                if (m_RightMarkerPosition == value)
+                    return;
+                m_RightMarkerPosition = value;
+                OnPropertyChanged("RightMarkerPosition");
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
