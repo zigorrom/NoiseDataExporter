f5c745e4429cb36c3c5d204e31ee1c198694ad9d
zigorrom <zigorrom@gmail.com>
2015-03-22 20:00:49 +0100

    123

--- NoiseDataExporter/NoiseDataExporter/MainWindow.xaml.cs
+++ NoiseDataExporter/NoiseDataExporter/MainWindow.xaml.cs
@@ -178,6 +178,8 @@ namespace NoiseDataExporter
                         }
                     }
                 }
+                if (CurrentList.Count > 0)
+                    m_queue.Enqueue(CurrentList);
 
             }
         }
